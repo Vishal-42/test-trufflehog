e1255867c7ca8fdfb37a603ca255634816289dd1
Vishal-42 <gamitvishal42@gmail.com>
2025-03-06 16:37:27 +0530

    more secrets

--- test.py
+++ test.py
@@ -1,3 +1,6 @@
 SECRET_KEY = '4e&6bz+(5&cg^_!05r(&7_#dghg_smrojq(yk)xa^bwg7j)^*j'
 KEY = '0fXmr002SuelwlPgNWs1FIiMSRcRvm9e'
 PASSWORD = "PASS@!23"
+
+MONGODB = "mongodb+srv://my-user:PASS@!23@clustername.mongodb.net/"
+NETLIFY = "abc12345abc12345abc12345abc12345abc12345abc"
