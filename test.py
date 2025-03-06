722d9be528fbaf3b661f4f7596362f6173a4e7b7
Vishal-42 <gamitvishal42@gmail.com>
2025-03-06 17:12:32 +0530

    TEST

--- test.py
+++ test.py
@@ -1,18 +1,12 @@
 SECRET_KEY = '4e&6bz+(5&cg^_!05r(&7_#dghg_smrojq(yk)xa^bwg7j)^*j'
 KEY = '0fXmr002SuelwlPgNWs1FIiMSRcRvm9e'
-
 USER = "Vishal"
 PASSWORD = "PASS@!23"
-
 MONGODB_CONNECTION = "mongodb+srv://my-user:PASS@!23@clustername.mongodb.net/"
-
 PASS = "abc12345abc12345abc12345abc12345abc12345abc"
-
 def login(user, password):
     if user==USER and password==PASSWORD:
         return True
     else:
         return False
-    
-
 login(user="Vishal", password="PASS@!23")
